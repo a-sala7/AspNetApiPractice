ad7e33e7f2474a61b28ca8382ee942cffffe7d15
Ahmed Salah <a.salah2727@gmail.com>
2023-06-19 14:08:03 +0300

    Add maxlength to user address, fname, lname

--- AspNetApiPractice.Models/User/AppUser.cs
+++ AspNetApiPractice.Models/User/AppUser.cs
@@ -7,12 +7,15 @@ namespace AspNetApiPractice.Models.User
     public class AppUser : IdentityUser
     {
         [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
   
         [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
         
         [Required]
+        [MaxLength(200)]
         public string Address { get; set; }
         public virtual ICollection<Product> Wishlist { get; set; }
     }
