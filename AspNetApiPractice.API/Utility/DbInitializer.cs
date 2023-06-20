11fbfc615f0cdaf5cdf65a85a54d9dff3148e6b2
Ahmed Salah <a.salah2727@gmail.com>
2023-06-20 10:58:23 +0300

    fix: DbInitializer

--- AspNetApiPractice.API/Utility/DbInitializer.cs
+++ AspNetApiPractice.API/Utility/DbInitializer.cs
@@ -1,69 +1,65 @@
 ﻿using AspNetApiPractice.Data;
 using AspNetApiPractice.Models.Shop;
 
-namespace AspNetApiPractice.API.Utility
+namespace AspNetApiPractice.API.Utility;
+public static class DbInitializer
 {
-    public static class DbInitializer
+    private static Product[] products = new Product[]
     {
-        public static void Initialize(WebApplication app) 
+        new()
         {
-            var dbContext = app
-                .Services
-                .CreateScope()
-                .ServiceProvider
-                .GetService<Entities>();
-
-            if (dbContext.Products.Any() || dbContext.Categories.Any())
-                return;
-
-            dbContext.Categories.AddRange(categories);
-            dbContext.Products.AddRange(products);
-
-            dbContext.SaveChanges();
+            Name_Ar = "اااالللل",
+            Name_En = "Dummy",
+            Description_Ar = "تفاصيل",
+            Description_En = "Desc",
+            Price = 129.99M
+        },
+        new()
+        {
+            Name_Ar = "اااالللل 2",
+            Name_En = "Dummy 2",
+            Description_Ar = "تفاصيل 2",
+            Description_En = "Desc 2",
+            Price = 15.99M
+        },
+        new()
+        {
+            Name_Ar = "اااالللل 3",
+            Name_En = "Dummy 3",
+            Description_Ar = "تفاصيل 3",
+            Description_En = "Desc 3",
+            Price = 21.99M
         }
+    };
 
-        private static Category[] categories =
+    private static Category[] categories = new Category[]
+    {
+        new Category()
         {
-            new Category()
-            {
-                Name_Ar = "صنف 1",
-                Name_En = "Category 1"
-            },
-            new Category()
-            {
-                Name_Ar = "صنف 2",
-                Name_En = "Category 2"
-            }
-        };
-        private static Product[] products =
+            Name_Ar = "صنف 1",
+            Name_En = "Category 1",
+            Products = new Product[] { products[0], products[1] }
+        },
+        new Category()
         {
-            new()
-            {
-                Name_Ar = "اااالللل",
-                Name_En = "Dummy",
-                Description_Ar = "تفاصيل",
-                Description_En = "Desc",
-                Price = 129.99M,
-                CategoryId = 1
-            },
-            new()
-            {
-                Name_Ar = "اااالللل 2",
-                Name_En = "Dummy 2",
-                Description_Ar = "تفاصيل 2",
-                Description_En = "Desc 2",
-                Price = 15.99M,
-                CategoryId = 1
-            },
-            new()
-            {
-                Name_Ar = "اااالللل 3",
-                Name_En = "Dummy 3",
-                Description_Ar = "تفاصيل 3",
-                Description_En = "Desc 3",
-                Price = 21.99M,
-                CategoryId = 2
-            }
-        };
+            Name_Ar = "صنف 2",
+            Name_En = "Category 2",
+            Products = new Product[] { products[2] }
+        }
+    };
+    public static void Initialize(WebApplication app) 
+    {
+        var dbContext = app
+            .Services
+            .CreateScope()
+            .ServiceProvider
+            .GetService<Entities>();
+
+        if (dbContext.Products.Any() || dbContext.Categories.Any())
+            return;
+
+        dbContext.Categories.AddRange(categories);
+
+        dbContext.SaveChanges();
     }
 }
