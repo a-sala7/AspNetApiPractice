af608d284f28bb2dcad7632d60f58511ef898f2f
Ahmed Salah <a.salah2727@gmail.com>
2023-06-22 11:53:27 +0300

    Add user ID to LoginResponse

--- AspNetApiPractice.ViewModels/User/LoginResponse.cs
+++ AspNetApiPractice.ViewModels/User/LoginResponse.cs
@@ -4,6 +4,7 @@ public class LoginResponse
 {
     public string Token { get; set; } = null!;
     public DateTime Expiration { get; set; }
+    public string Id { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
